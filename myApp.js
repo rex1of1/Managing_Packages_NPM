99ec92eb597b186821deeadf248e79d5dc954afc
rex1of1 <107004092+rex1of1@users.noreply.github.com>
2022-08-05 01:54:35 -0500

    organize function to myApp.js

--- myApp.js
+++ myApp.js
@@ -11,28 +11,16 @@ var bGround = require('fcc-express-bground');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 bGround.log("Hello World");
 console.log("Hello World");
+
+
+app.get("/", function (reg, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
