db48fae7b09f8c1f336f539d42b0b599c8b696cd
rex1of1 <107004092+rex1of1@users.noreply.github.com>
2022-08-05 22:01:42 -0500

    modified

--- myApp.js
+++ myApp.js
@@ -29,16 +29,13 @@ app.use("/public", express.static(__dirname + "/public"));
 //});
 
 
-app.get("/json", (req,res) => {
-    if(process.env.MESSAGE_STYLE === "uppercase") {
-        res.json(
-            {"message": "HELLO JSON"}
-        )
-    } else {
-        res.json(
-            {"message": "Hello json"}
-        )
+app.get("/json", function(req, res){
+    var jsonResponse = {"message": "Hello json"};
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        jsonResponse.message = jsonResponse.message.toUpperCase()
     }
+    res.json(jsonResponse)
 });
 
 
